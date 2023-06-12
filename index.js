f94fb2ebcf4f8caced53d4d818edf626a5b0daac
Ezaz Ahmed <ezazrahul794@gmail.com>
2023-06-12 22:49:06 +0600

    'test'

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ const client = new MongoClient(uri);
 
 async function run() {
   try {
-    await client.connect();
+    client.connect();
 
     const servicesCategories = client.db('products').collection('categories');
     const servicesProducts = client.db('products').collection('product');
