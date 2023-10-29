97ebb2dc76c589ea2c5cf65739d5f82e89af2769
Ezaz Ahmed <ezazrahul794@gmail.com>
2023-10-29 23:44:04 +0600

    'fix'

--- index.js
+++ index.js
@@ -7,11 +7,7 @@ const port = process.env.PORT || 5000;
 require("dotenv").config();
 
 // Middleware
-app.use(cors({
-  origin: 'http://localhost:3000', 
-  methods: 'GET, POST, PUT, DELETE',
-  allowedHeaders: 'Content-Type',
-}));
+app.use(cors());
 app.use(express.json());
 
 
